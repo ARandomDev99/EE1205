09c99fca5ccd94a6a8751be69052d58786d184a6
Aneesh Kadiyala <143342960+ARandomDev99@users.noreply.github.com>
2024-01-05 22:08:04 +0530

    Correct graph title

--- NCERT-discrete 10.5.2.19/codes/10_5_2_19.py
+++ NCERT-discrete 10.5.2.19/codes/10_5_2_19.py
@@ -2,10 +2,10 @@ import numpy
 import matplotlib.pyplot as plt
 
 # X-axis
-n = numpy.arange(1, 31, 1)
+n = numpy.arange(0, 31, 1)
 
 # Y-axis
-x = 4800 + 200 * n
+x = 5000 + 200 * n
 
 # Plot graph
 plt.stem(n, x)
@@ -15,7 +15,7 @@ plt.xlabel("n")
 plt.ylabel("x(n)")
 
 # Title
-plt.title("$x(n) = 4800 + 200n$")
+plt.title("$x(n) = 5000 + 200n$")
 
 # Save x(n) vs n graph
 plt.savefig("../figs/10_5_2_19.png")
