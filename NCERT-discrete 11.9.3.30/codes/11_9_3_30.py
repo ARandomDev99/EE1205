c4c0ff22775c7bd99e4e3f1a9ebe0be64ec28ff6
Aneesh Kadiyala <143342960+ARandomDev99@users.noreply.github.com>
2024-01-05 22:08:46 +0530

    Move second question to separate directory

--- NCERT-discrete 11.9.3.30/codes/11_9_3_30.py
+++ NCERT-discrete 11.9.3.30/codes/11_9_3_30.py
@@ -2,10 +2,10 @@ import numpy
 import matplotlib.pyplot as plt
 
 # X-axis
-n = numpy.arange(1, 11, 1)
+n = numpy.arange(0, 11, 1)
 
 # Y-axis
-x = 15 * numpy.power(2, n)
+x = 30 * (2 ** n)
 
 # Plot graph
 plt.stem(n, x)
@@ -15,6 +15,6 @@ plt.xlabel("n")
 plt.ylabel("x(n)")
 
 # Title
-plt.title("$x(n) = 15(2^n)$")
+plt.title("$x(n) = 30(2^n)$")
 
-plt.savefig("../plots/11_9_3_30.png")
+plt.savefig("../figs/11_9_3_30.png")
