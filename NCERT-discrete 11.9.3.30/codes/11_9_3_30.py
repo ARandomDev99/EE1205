51f62716da980f53ad357b5948347a4c7f5c3ef6
Aneesh Kadiyala <143342960+ARandomDev99@users.noreply.github.com>
2024-01-20 12:20:58 +0530

    Remove unnecessary equations and move table to separate directory

--- NCERT-discrete 11.9.3.30/codes/11_9_3_30.py
+++ NCERT-discrete 11.9.3.30/codes/11_9_3_30.py
@@ -2,13 +2,18 @@ import numpy
 import matplotlib.pyplot as plt
 
 # X-axis
-n = numpy.arange(0, 11, 1)
+n = numpy.linspace(0, 7, 8)
 
 # Y-axis
 x = 30 * (2 ** n)
 
 # Plot graph
 plt.stem(n, x)
+
+# Plot (2, 120) and (4, 480) in a separate color and mark them.
+plt.stem([2, 4], [120, 480], linefmt="C3")
+plt.annotate("120", (2, 120), ha="center", va="bottom", xytext=(0, 10), textcoords="offset points")
+plt.annotate("480", (4, 480), ha="center", va="bottom", xytext=(0, 10), textcoords="offset points")
 
 # Label axes
 plt.xlabel("n")
