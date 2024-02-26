c85fba3eb30a2e9aa5dc5c302b5cc6d6a1bae0d5
ARandomDev99 <ee23btech11007@iith.ac.in>
2024-02-26 22:06:08 +0530

    Include negative time in plot and use DC only for steady state analysis

--- GATE-2023-BM-30/codes/plot.py
+++ GATE-2023-BM-30/codes/plot.py
@@ -1,18 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-t = np.linspace(0, 1, 100)
+# X-axis
+t = np.linspace(-.2, 1, 200)
 
-v_laplace = 7 * (1 - np.exp(-t / .121))
+# Y-axis
+# V_c = 7u(t)(1 - e^(-t / 0.121))
+V_c = 7 * np.heaviside(t, 1) * (1 - np.exp(-t / .121))
 
-v_dc = 7 * (1 - np.exp(-t / .121))
-
-plt.plot(t, v_laplace, label="s-Domain", linestyle="-.", color="C0")
-plt.plot(t, v_dc, label="DC", linestyle=":", color="C3")
+plt.plot(t, V_c)
 
 plt.xlabel("Time ($s$)")
 plt.ylabel("Voltage ($V_c$)")
-
-plt.legend()
 
 plt.savefig("plot.png")
